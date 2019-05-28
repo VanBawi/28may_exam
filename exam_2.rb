13eaf0974cd72811e0eac4a71d73660f17456ef4
Joel Lim <akaru@littlix.com>
2019-05-28 09:40:01 +0800

    Initial Commit

---
+++ exam_2.rb
@@ -0,0 +1,16 @@
+# You will write a method reverse(word) that takes in a String
+# The method returns the word with its letters in reverse order
+# You may not use the String#reverse or String#reverse! method
+
+def reverse(word)
+    # Write your code here
+end
+
+puts reverse("cat") == "tac"
+puts
+
+puts reverse("programming") == "gnimmargorp"
+puts
+
+puts reverse("bootcamp") == "pmactoob"
+puts
